f1aed514b05148693e5ff437ec0ca49bec47ad8c
a-rvid <git@rvid.eu>
2026-02-18 17:38:05 +0000

    FriskoLAN

--- bot.py
+++ bot.py
@@ -152,7 +152,7 @@ class MyModal(disnake.ui.Modal):
 
     # The callback received when the user input is completed.
     async def callback(self, inter: disnake.ModalInteraction):
-        embed = disnake.Embed(title="Anmälan", description="Anmälan registrerad!")
+        embed = disnake.Embed(title="FriskoLAN Anmälan", description="Anmälan registrerad!")
         for key, value in inter.text_values.items():
             embed.add_field(
                 name=key.capitalize(),
