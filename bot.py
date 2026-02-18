5ffb729131b259c16c03a6083f6a492566471a94
a-rvid <git@rvid.eu>
2026-02-18 16:54:24 +0000

    modal?

--- bot.py
+++ bot.py
@@ -126,6 +126,43 @@ async def anmalan(inter, namn: str, klass: str, preferenser: str = "inga"):
     print(f"Namn: {namn}, Klass: {klass}, preferenser: {preferenser}")
     await inter.response.send_message("Uppfattat! Intresseanmälan registrerad.")
 
+class MyModal(disnake.ui.Modal):
+    def __init__(self):
+        # The details of the modal, and its components
+        components = [
+            disnake.ui.TextInput(
+                label="Name",
+                placeholder="Foo Tag",
+                custom_id="name",
+                style=TextInputStyle.short,
+                max_length=50,
+            ),
+            disnake.ui.TextInput(
+                label="Description",
+                placeholder="Lorem ipsum dolor sit amet.",
+                custom_id="description",
+                style=TextInputStyle.paragraph,
+            ),
+        ]
+        super().__init__(title="Create Tag", components=components)
+
+    # The callback received when the user input is completed.
+    async def callback(self, inter: disnake.ModalInteraction):
+        embed = disnake.Embed(title="Tag Creation")
+        for key, value in inter.text_values.items():
+            embed.add_field(
+                name=key.capitalize(),
+                value=value[:1024],
+                inline=False,
+            )
+        await inter.response.send_message(embed=embed)
+
+
+@bot.slash_command()
+async def tags(inter: disnake.AppCmdInter):
+    """Sends a Modal to create a tag."""
+    await inter.response.send_modal(modal=MyModal())
+
 @bot.slash_command(description="get the latency of the bot")
 async def ping(ctx):
     latency = bot.latency * 1000
